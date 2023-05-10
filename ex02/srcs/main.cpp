833b92a082d9a786159c7f0cea1834b7e96de034
Pierre Franceschi <pfrances@student.42tokyo.jp>
2023-05-10 23:02:40 +0900

    update

--- ex02/srcs/main.cpp
+++ ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: pfrances <pfrances@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/05 16:29:05 by pfrances          #+#    #+#             */
-/*   Updated: 2023/05/10 22:36:10 by pfrances         ###   ########.fr       */
+/*   Updated: 2023/05/10 23:02:22 by pfrances         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ int	main(int argc, char **argv) {
 		return 1;
 	}
 	if (argc > 10001) {
-		std::cout << "error: too many numbers to sort" << std::endl;
+		std::cout << "error: too many numbers to sort. [Max: 10 000]" << std::endl;
 		return 1;
 	}
 	PmergeMe::MergeSort(argv);
